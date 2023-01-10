ec513d67ea4bc2259f55e2f5229fbf215e801b3d
Christian Morris <116536697+DevChristianMorris@users.noreply.github.com>
2023-01-10 15:36:09 +1300

    Add files via upload

---
+++ 02-exercise---add-auth0-to-your-application-ChristianMorrisDI-main/server/src/app.js
@@ -0,0 +1,73 @@
+const mongoose = require("mongoose");
+const express = require("express");
+const cors = require("cors");
+const { celebrate, Joi, errors, Segments } = require("celebrate");
+const PropertyModel = require("./models/PropertyModel");
+const formatProperty = require("./formatProperty");
+const { auth } = require("express-oauth2-jwt-bearer");
+const checkJwt = auth({
+  audience: "https://madeup/url",
+  issuerBaseURL: `https://dev-w2k5dp8p.us.auth0.com/`,
+});
+
+const app = express();
+
+app.use(cors());
+
+app.use(express.json());
+
+app.post(
+  "/properties",
+  checkJwt,
+  celebrate({
+    [Segments.BODY]: Joi.object().keys({
+      description: Joi.string().required(),
+      address: Joi.string().required(),
+      title: Joi.string().required(),
+      img: Joi.string().required(),
+      askingPrice: Joi.number().min(0).required(),
+    }),
+  }),
+  async (req, res, next) => {
+    try {
+      const { body, auth } = req;
+      const document = {
+        createdBy: auth.payload.sub,
+
+        ...body,
+      };
+
+      const property = new PropertyModel(document);
+      await property.save();
+      return res.status(201).send(formatProperty(property));
+    } catch (error) {
+      error.status = 400;
+      next(error);
+    }
+  }
+);
+
+app.get("/properties", async (req, res) => {
+  const properties = await PropertyModel.find({});
+  return res.status(200).send(properties.map(formatProperty));
+});
+
+app.get("/properties/:id", async (req, res) => {
+  const { id } = req.params;
+
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(400).send({ error: "bad id" });
+  }
+
+  const property = await PropertyModel.findById(id);
+
+  if (property === null) {
+    return res.status(404).send("not found");
+  }
+
+  return res.status(200).send(formatProperty(property));
+});
+
+app.use(errors());
+
+module.exports = app;
